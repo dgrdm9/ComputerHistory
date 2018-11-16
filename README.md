62bfe53fbe5d09de20f2fe7b1da863be112cd9cd
dgrdm9 <dgrdm9@gmail.com>
2018-11-16 19:11:00 +0100

    Few changes

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-## MenuList
+## Computer History
 It's an application to study the history of computers in a very boring way. 
--- app/src/main/java/com/dgrdm9/menulist/Fragments/HomeFragment.java
+++ app/src/main/java/com/dgrdm9/menulist/Fragments/HomeFragment.java
@@ -6,14 +6,17 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.dgrdm9.menulist.R;
+import com.squareup.picasso.Picasso;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class HomeFragment extends Fragment {
 
+    private ImageView imageView;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -25,6 +28,8 @@ public class HomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_home, container, false);
+        imageView = v.findViewById(R.id.imageView2);
+        Picasso.get().load(R.drawable.computacion).fit().centerCrop().into(imageView);
         return v;
     }
 
