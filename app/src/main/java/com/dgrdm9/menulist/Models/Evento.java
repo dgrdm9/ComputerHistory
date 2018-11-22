a9d005db39fa89993da9366cbd4da1664203a333
dgrdm9 <dgrdm9@gmail.com>
2018-11-22 21:56:54 +0100

    Still developing it
    Added:
    - Event Model
    - Home Screen Text
    - Few layout improvements

---
+++ app/src/main/java/com/dgrdm9/menulist/Models/Evento.java
@@ -0,0 +1,31 @@
+package com.dgrdm9.menulist.Models;
+
+public class Evento {
+    private String fecha;
+    private String descripcion;
+
+    public Evento() {
+    }
+
+    public Evento(String fecha, String descripcion) {
+        this.fecha = fecha;
+        this.descripcion = descripcion;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+}
+
